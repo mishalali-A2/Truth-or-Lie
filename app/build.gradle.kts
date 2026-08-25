3954f42283d7a64b11a29a04ecab5b5ba0cf3596
haa1117 <53368850+haa1117@users.noreply.github.com>
2026-08-25 11:40:20 +0500

    build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.futurewatch.truthorlietv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.10"
 
     }
 
